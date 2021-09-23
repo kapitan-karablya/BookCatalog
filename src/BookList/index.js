a7d0ae097bfa86f287ffdda36b7e909fb4fa9c15
kapitan-karablya <58504325+kapitan-karablya@users.noreply.github.com>
2021-09-24 00:28:30 +0500

    Добавил незначительные улучшения интерфейса

--- src/BookList/index.js
+++ src/BookList/index.js
@@ -4,7 +4,11 @@ import Book from "../Book";
 
 function BookList(props) {
 
+    if (localStorage.length < 2)
+        return <div className="empty_list"><p>Книг пока нет</p><p>Добавьте книги через соответствующую форму</p></div>
+
     return (
+
         <div className="book-list">
             {Object.keys(props.books).slice(0).reverse().map(bookId => (
                 <Book key={bookId} id={bookId} update={props.update} clickEditButton={props.clickEditButton}/>
